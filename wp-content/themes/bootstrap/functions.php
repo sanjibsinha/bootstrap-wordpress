d158c684e580619284deafa9bdb320c7e89a7b31
Sanjib <sanjib12sinha@gmail.com>
2021-07-10 13:19:15 +0530

    updating front page

--- wp-content/themes/bootstrap/functions.php
+++ wp-content/themes/bootstrap/functions.php
@@ -22,3 +22,16 @@ function set_excerpt_length() {
 }
 
 add_filter('excerpt_length', 'set_excerpt_length');
+
+// fetching all styles, fonts and js
+
+function fetch_bootstrap_files() {
+    wp_enqueue_script('bootstrap-js', get_theme_file_uri('/js/bootstrap.js',), NULL, '1.0', true);
+    // wp_enqueue_style('google-custom-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    // wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('bootstrap_main_style', get_stylesheet_uri());
+}
+
+add_action('wp_enqueue_scripts', 'fetch_bootstrap_files');
+
+add_theme_support('post-thumbnails');
--- wp-content/themes/bootstrap/header.php
+++ wp-content/themes/bootstrap/header.php
@@ -52,11 +52,11 @@
 <div class="container">
   <header class="blog-header py-3">
     <div class="row flex-nowrap justify-content-between align-items-center">
-      <div class="col-4 pt-1">
+      <!-- <div class="col-4 pt-1">
         <a class="link-secondary" href="#">Subscribe</a>
-      </div>
+      </div> -->
       <div class="col-4 text-center">
-        <a class="blog-header-logo text-dark" href="#">Sanjib</a>
+        <a class="blog-header-logo text-dark" href="#">Sanjib Sinha</a>
       </div>
       <div class="col-4 d-flex justify-content-end align-items-center">
         <a class="link-secondary" href="#" aria-label="Search">
